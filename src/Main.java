99343b491c5a255b5b76494ea69c4c1956c8f345
Dmitrii Odynets <dimosaintp@gmail.com>
2024-04-24 16:30:06 +0300

    Ready.

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Объявляем переменные для входных данных.
+
+        double account = 500; // Начальная сумма на счёте.
+        double amount = 1200; // Сумма пополнения счёта.
+
+        // Вычисляем количество бонусов и итоговую сумму на счёте с учетом бонусов.
+
+        if (amount > 1000) {
+            int bonus = (int) (amount / 100); // Количество бонусов.
+            int result = (int) (account + amount + bonus); // Итоговая сумма на счёте.
+            System.out.println("Бонусов начислено: " + bonus); // Выводим количество бонусов.
+            System.out.println("Сумма на счёте: " + result); // Выводим итоговую сумму на счёте с учетом бонусов.
+        } else {
+            if (amount < 1000) {
+                int bonus = (int) (0);
+                int result = (int) (account + amount + bonus);
+                System.out.println("Бонусов не начислено");
+                System.out.println("Сумма на счёте: " + result);
+            }
+        }
+    }
+}
