ffcbc40e5bc6e870acc4b4f95b793d4272017c11
Dmitrii Odynets <dimosaintp@gmail.com>
2024-04-25 10:39:54 +0300

    Correction.

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         // Объявляем переменные для входных данных.
 
         double account = 500; // Начальная сумма на счёте.
-        double amount = 1200; // Сумма пополнения счёта.
+        double amount = 900; // Сумма пополнения счёта.
 
         // Вычисляем количество бонусов и итоговую сумму на счёте с учетом бонусов.
 
@@ -14,12 +14,10 @@ public class Main {
             System.out.println("Бонусов начислено: " + bonus); // Выводим количество бонусов.
             System.out.println("Сумма на счёте: " + result); // Выводим итоговую сумму на счёте с учетом бонусов.
         } else {
-            if (amount < 1000) {
-                int bonus = (int) (0);
-                int result = (int) (account + amount + bonus);
-                System.out.println("Бонусов не начислено");
-                System.out.println("Сумма на счёте: " + result);
-            }
+            int bonus = (int) (0);
+            int result = (int) (account + amount + bonus);
+            System.out.println("Бонусов не начислено");
+            System.out.println("Сумма на счёте: " + result);
         }
     }
 }
